f3b1ad7fb5a214aa30cc337684563c92f6a5174d
superTUX <supertux@cupdev.net>
2016-04-27 15:41:36 +0200

    removed obsolete lines

--- main.py
+++ main.py
@@ -5,24 +5,17 @@ import os
 import time
 
 
-def pos(p):
-    return "\x10" + chr(p)
-
 def getstuff(cmd):
     return [s.strip("\n") for s in os.popen(cmd).readlines()]
-
 
 d1 = d.display('/dev/ttyUSB0', 2400)
 d1.cursor(0)
 
 mpc_cmd = "mpc -h 192.168.0.2 -P password"
-#song = scrolltext("ganz langer text der devinitiv laenger als 40 zeichen ist", 39)
-#song = d.scrolltext(0, 39, "foo")
-#prog = d.pbar(40, 39, 0)
+
 d1.add("song", d.scrolltext(0, 39, "foo"))
 d1.add("prog", d.pbar(40, 39, 0))
 
-#p = int(re.search("\d+", re.search("\(\d*\%\)", os.popen(mpc_cmd).readlines()[1]).group()).group())
 
 while 1:
     #date = rpad(getstuff("date"), 40)
