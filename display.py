4a8d2c736b70ce6160a9fd883a8010267b91417d
supertux <supertux@cupdev.net>
2016-05-31 12:30:36 +0200

    fixed crash on non-ascii characters

--- display.py
+++ display.py
@@ -1,27 +1,43 @@
 import serial
-import io
-import os
-import time
-import re
 
 def rpad(s, l):
     if(len(s) < l):
         s += (l - len(s)) * " "
         return s
+    return s
+
+def lpad(s, l):
+    if(len(s) < l):
+        s = (l - len(s)) * " " + s
+        return s
+    return s
+
+def mpad(s, l):
+    if(len(s) < l):
+        r = l - len(s)
+        s = int(.5 * r) * " " + s + (r - int(.5 * r)) * " "
+        return s
+    return s
 
 class display(object):
     """docstring for display"""
     def __init__(self, port, baud):
         super(display, self).__init__()
         self.ser = serial.Serial(port, baud)
-        self.sio = io.TextIOWrapper(io.BufferedRWPair(self.ser, self.ser))
         self.printd('\x1F')
+        self.fields = {}
     def printd(self, str):
-        self.sio.write(str)
-        self.sio.flush()
+        self.ser.write(str.encode('ascii', 'ignore'))
+        self.ser.flush()
     def cursor(self, b):
         if(b): self.printd('\x13')
         else: self.printd('\x14')
+    def update(self):
+        [self.printd(f.getstring()) for f in self.fields.values()]
+    def add(self, name, f):
+        self.fields[name] = f
+    def remove(self, name):
+        self.fields.pop(name)
 
 class field(object):
     """docstring for field"""
@@ -29,24 +45,45 @@ class field(object):
         super(field, self).__init__()
         self.pos = position
         self.len = length
+        self.visible = True
     def getstring(self):
-        return "\x10" + chr(self.pos) + self.prepstring()
+        if(self.visible):
+            return "\x10" + chr(self.pos) + self.prepstring()
 
-class scrolltext(field):
+class text(field):
+    """docstring for text"""
+    def __init__(self, position, length, textstr = "", bound = "l"):
+        super(text, self).__init__(position, length)
+        self.bound = bound
+        self.change(textstr)
+    def change(self, text):
+        if((self.bound == "r") | (self.bound == "right")):
+            self.text = lpad(text, self.len)
+        elif((self.bound == "l") | (self.bound == "left")):
+            self.text = rpad(text, self.len)
+        elif((self.bound == "m") | (self.bound == "middle")):
+            self.text = mpad(text, self.len)
+        else:
+            raise Exception("fuck you!")
+    def prepstring(self):
+        return(self.text[0:self.len])
+
+
+class scrolltext(text):
     """docstring for scrolltext"""
-    def __init__(self, position, length, text):
-        super(scrolltext, self).__init__(position, length)
+    def __init__(self, position, length, text, bound):
+        super(scrolltext, self).__init__(position, length, text, bound)
         self.spos = 0
-        self.text = rpad(text, length)
+        self.change(text)
     def shift(self):
         self.spos += 1
         if(self.spos > len(self.text) - self.len): self.spos = 0
     def prepstring(self):
         return self.text[self.spos:self.spos+self.len]
-    def update(self, newtext):
+    def change(self, newtext):
+        super(scrolltext, self).change(newtext)
         if(newtext != self.text):
             self.spos = 0
-            self.text = rpad(newtext, self.len)
 
 class pbar(field):
     """docstring for pbar"""
@@ -56,5 +93,5 @@ class pbar(field):
     def prepstring(self):
         p = int(self.val * self.len / 100)
         return p * "#" + (self.len - p) * "-"
-    def update(self, value):
+    def change(self, value):
         self.val = value
--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
-import display
-
-
-def pos(p):
-    return "\x10" + chr(p)
+import display as d
+import re
+import os
+import time
 
 def getstuff(cmd):
-    return os.popen(cmd).readlines()[0].strip("\n")
+    return [s.strip("\n") for s in os.popen(cmd).readlines()]
 
-
-d1 = display('/dev/ttyUSB0', 2400)
+d1 = d.display('/dev/ttyUSB0', 2400)
 d1.cursor(0)
 
-mpc_cmd = "mpc -h 192.168.0.2 -P password"
-#song = scrolltext("ganz langer text der devinitiv laenger als 40 zeichen ist", 39)
-song = scrolltext(0, 39, "")
-prog = pbar(40, 39, 0)
+mpc_cmd = "mpc -h 192.168.0.2 -P password | unidecode -e utf8"
 
-#p = int(re.search("\d+", re.search("\(\d*\%\)", os.popen(mpc_cmd).readlines()[1]).group()).group())
+d1.add("song", d.scrolltext(0, 39, "foo", "m"))
+d1.add("prog", d.pbar(45, 29, 0))
+d1.add("curtime", d.text(40, 5, "", "l"))
+d1.add("tottime", d.text(74, 5, "", "r"))
+# d1.add("pause", d.text(55, 10, " [paused] "))
+timer = 0
 
 while 1:
     #date = rpad(getstuff("date"), 40)
-    prog.update(int(re.search("\d+", re.search("\(\d*\%\)", os.popen(mpc_cmd).readlines()[1]).group()).group()))
-    song.update(getstuff(mpc_cmd))
+    status = getstuff(mpc_cmd)
+    d1.fields["curtime"].change(re.search("(\d+:\d+)\/(\d+:\d+)", status[1]).group(1))
+    d1.fields["tottime"].change(re.search("(\d+:\d+)\/(\d+:\d+)", status[1]).group(2))
+    d1.fields["prog"].change(int(re.search("\((\d+)\%\)", status[1]).group(1)))
+    d1.fields["song"].change(status[0])
     #d1.printd(pos(0) + date)
-    d1.printd(prog.getstring())
-    d1.printd(song.getstring())
-    song.shift()
+    d1.update()
+    d1.fields["song"].shift()
     time.sleep(1)
